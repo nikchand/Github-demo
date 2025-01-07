dbbf9b875744e77a3f637df4197b82718a56887c
nikchand <127674289+nikchand@users.noreply.github.com>
2025-01-07 23:52:48 +0530

    Delete remove_all_containers.sh

--- remove_all_containers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "show all the containers"
-docker ps -a
-echo "stopping all running containers"
-docker stop $(docker ps -a -q)
-echo "removing all running containers"
-docker rm $(docker ps -a -q)
-echo "all the containers have been removed"
