ca09d4deae9da9533ee962fbe00cb415f23bdcdf
Nikita <nikitachandrakar26@gmail.com>
2025-02-09 23:35:02 +0530

    Ansible notes

--- Docker_Install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo yum update -y
-sudo yum install docker -y
-sudo usermod -aG docker ec2-user
-newgrp docker
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo systemctl status docker
